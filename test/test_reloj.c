100e2d3fe956b14b4b9e44fb430fdaa4ea6aa475
Joel Jassan <joeljassan@hotmail.com>
2023-06-10 12:55:31 -0300

    Se testea ceedling

---
+++ test/test_reloj.c
@@ -0,0 +1,17 @@
+Al inicializar el reloj está en 00:00 y con hora invalida.
+‣ Al ajustar la hora el reloj queda en hora y es válida.
+‣ Después de n ciclos de reloj la hora avanza un segundo,
+diez segundos, un minutos, diez minutos, una hora, diez
+horas y un día completo.
+‣ Fijar la hora de la alarma y consultarla.
+‣ Fijar la alarma y avanzar el reloj para que suene.
+‣ Fijar la alarma, deshabilitarla y avanzar el reloj para no
+suene.
+‣ Hacer sonar la alarma y posponerla.
+‣ Hacer sonar la alarma y cancelarla hasta el otro dia..
+
+#include "unity.h"
+
+void test_inicializacion(void) {
+    TEST_FAIL_MESSAGE ("Funciona!");
+}
